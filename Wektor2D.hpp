88883bb037e8ab915ec935e1423c11ea18780fef
BartekFranczak <91423411+BartekFranczak@users.noreply.github.com>
2022-10-27 13:05:16 +0200

    Update Wektor2D.hpp

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,35 +1,37 @@
 class Wektor2D
 {
-    // Tutaj napisz implementacje klasy Wektor2D
-    Wektor2D(){x=0.;y=0.;}
+    public:
+    
+    
+    
     Wektor2D(double xx, double yy)
     {
         setX(xx);
         setY(yy);
     }
+    Wektor2D(){x=0.;y=0.;}
+    
+    ~Wektor2D(){}
+    void    setX(double a){x=a;}
+    double  getX() {return x;}
+    
+    void    setY(double a){y=a;}
+    double  getY() {return y;}
+  
+    Wektor2D operator +(Wektor2D b)
+    {
+        double xx = x + b.getX();
+        double yy = y + b.getY();
+        return Wektor2D {xx,yy};
+    }
 
-    ~Wektor2D()
-    {   
-    } 
-    public:
-        void    setX(double a){x=a;}
-        double  getX() {return x;}
-        void    setY(double a){y=a;}
-        double  getY() {return y;}
-        
+    double operator *(Wektor2D b)
+    {
+        return x*b.getX() + y*b.getY(); 
+    }      
     private:
         double x;
         double y;
 };
 
-Wektor2D operator +(Wektor2D a, Wektor2D b)
-{
-    double xx = a.getX() + b.getX();
-    double yy = a.getY() + b.getY();
-    return Wektor2D {xx,yy};
-}
 
-double operator *(Wektor2D a, Wektor2D b)
-{
-    return a.getX()*b.getX()+a.getY()*b.getY(); 
-}
