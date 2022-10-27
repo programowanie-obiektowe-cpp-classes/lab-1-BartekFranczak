c08cfe76e528c96566b1dfc14f08176e84e11f29
BartekFranczak <91423411+BartekFranczak@users.noreply.github.com>
2022-10-27 12:22:25 +0200

    Update Wektor2D.hpp

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,4 +1,25 @@
 class Wektor2D
 {
     // Tutaj napisz implementacje klasy Wektor2D
+    {   
+
+    Wektor2D(double xx=0, double yy=0)
+    {
+        setX(xx);
+        setY(yy);
+    }
+
+    ~Wektor2D()
+    {   
+    } 
+    public:
+        void    setX(double a){x=a;}
+        double  getX() {return x;}
+        void    setY(double a){y=a;}
+        double  getY() {return y;}
+        
+    private:
+        double x;
+        double y;
+};
 };
